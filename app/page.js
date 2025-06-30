9db4ed8e4c3b89b9af4396c5b037a65bddd533ed
Faizan Khan <fk790080@gmail.com>
2025-07-01 01:12:17 +0530

    Basic Setup

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import {Button} from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      Hello,Subscribe to me 
+      <br></br>
+      <Button variant = {"destructive"}>Subscribe</Button>
+      </div>
+  );
+}
