cc1f9d8d0592de8f43e11ae48a7c5962b7d7f118
Faizan Khan <fk790080@gmail.com>
2025-07-03 13:44:08 +0530

    Linked Convex with Clerk

--- components/header.jsx
+++ components/header.jsx
@@ -1,19 +1,31 @@
+"use client";
+
+import{ useStoreUser } from "@/hooks/use-store-user";
 import React from 'react';
 import { SignedOut, SignedIn, SignInButton, SignUpButton, UserButton} from "@clerk/nextjs";
+import { BarLoader } from "react-spinners";
+
+
 
 const Header = () => {
+  const { isLoading } = useStoreUser();
+
   return (
-    <div>
+    <header className="fixed top-0 w-full border-b bg-white/95 backdrop-blur z-50 supports-[backdrop-filter]:bg-white/60">
+      <nav>
        <SignedOut>
               <SignInButton />
               <SignUpButton>
                 
               </SignUpButton>
-            </SignedOut>
+        </SignedOut>
             <SignedIn>
               <UserButton />
             </SignedIn>
-    </div>
+      </nav>
+
+      {isLoading && <BarLoader width={"100%"} color="#36d7b7"/>}
+    </header>
   );
 };
 
